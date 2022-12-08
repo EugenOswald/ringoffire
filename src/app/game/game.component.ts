1debf5edecc35b235e2c8252ce85b2ec10a5632a
Eugen Oswald <oswaldeugen95@gmail.com>
2022-12-08 19:33:10 +0100

    Insert maps and logic

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss']
+  styleUrls: ['./game.component.scss'],
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  game: Game;
 
+  ngOnInit() {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game(); // die Variabel bekommt ein neues von dem, was wir angelegt haben.
+    console.log(this.game)
+  }
+
+  takeCard() {
+    this.pickCardAnimation = true;
+  }
 }
