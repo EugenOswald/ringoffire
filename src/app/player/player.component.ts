c8dd3507355cd0635957b4518b88527021543b7f
Eugen Oswald <oswaldeugen95@gmail.com>
2022-12-09 13:15:33 +0100

    show active player

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -4,10 +4,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
-  styleUrls: ['./player.component.scss']
+  styleUrls: ['./player.component.scss'],
 })
 export class PlayerComponent {
-
   @Input() name;
-
+  @Input() playerActive: boolean = false;
 }
