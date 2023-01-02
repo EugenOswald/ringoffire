6f5e6e1fc99d9a4258a391126309e60012d2d99e
Eugen Oswald <oswaldeugen95@gmail.com>
2023-01-02 19:22:27 +0100

    update firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'ring-of-fire-67434',
+    appId: '1:798832914:web:c2c63cd82ccb03f36b1bcb',
+    storageBucket: 'ring-of-fire-67434.appspot.com',
+    apiKey: 'AIzaSyDBHCg5Q3uhyJQTKJaiZE8R6Dt-54m_BBw',
+    authDomain: 'ring-of-fire-67434.firebaseapp.com',
+    messagingSenderId: '798832914',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
